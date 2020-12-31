a0797d8dd1ccefb5805eb46ac5af103132f6ec3f
shreyas-aher-cool <75071644+shreyas-aher-cool@users.noreply.github.com>
2020-12-31 16:20:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var car,wall;
+var speed,weight;
+
+function setup() {
+  createCanvas(1600,400);
+ 
+ speed=random(55,99);
+ weight=random(400,1500);
+ 
+  car=createSprite(400, 200, 50, 50);
+  car.velocityX=speed;
+ car.shapeColor="black";
+
+
+  
+  wall=createSprite(1500,200,60,height/2);
+ 
+
+}
+
+function draw() {
+  background(255,255,255);
+  
+  if(wall.x-car.x<(car.width+wall.width)/2){
+car.velocityX=0;
+var deformation=0.5*weight*speed*speed/22509;
+
+if(deformation>180){
+  car.shapeColor="green";
+}
+if(deformation<180 && deformation>100) {
+  car.shapeColor="yellow";
+}
+if(deformation>100){
+  car.shapeColor="red";
+
+}
+
+
+  }
+
+  drawSprites();
+}
